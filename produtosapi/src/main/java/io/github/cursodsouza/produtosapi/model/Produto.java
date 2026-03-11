7c3571d95d006b83ebda16afc9e35ea8ebcd6286
Alex Lima <alex.lima@jfl.com.br>
2026-03-11 17:35:32 -0300

    S2 - 14. Persistindo produtos no BD.

--- produtosapi/src/main/java/io/github/cursodsouza/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/io/github/cursodsouza/produtosapi/model/Produto.java
@@ -1,10 +1,25 @@
 package io.github.cursodsouza.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto") // Nome da tabela no BD se for diferente do nome dessa classe. Opcional de for o msm.
 // POJO -> Plain Old Java Object
 public class Produto {
+    @Id
+    @Column (name = "id") // Nome da coluna no BD se for diferente da propriedade dessa classe. Opcional de for o msm.
     private String id;
+
+    @Column (name = "nome")
     private String nome;
+
+    @Column (name = "descricao")
     private String descricao;
+
+    @Column (name = "preco")
     private Double preco;
 
     public String getId() {
