a46cf60e8b7a00ced9ac1fb89eae3443453ca50e
Alex Lima <alex.lima@jfl.com.br>
2026-04-27 23:49:52 -0300

    54. Mapeamento da entidade Livro e utilizamdo Lombok.

---
+++ S4_libraryapi/src/main/java/io/github/cursodsouza/libraryapi/model/Livro.java
@@ -0,0 +1,40 @@
+package io.github.cursodsouza.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data // incorpora os: getter e setters e outra outras deixando completo
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
+
+    //@Enumerated(EnumType.ORDINAL) para guardar a posicao de cada item do enum
+    @Enumerated(EnumType.STRING) // com o string nao corre o risco de mudar a ordem e assim começar a não bater com ja salvo no BD
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private BigDecimal preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
