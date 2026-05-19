67fb986bcbb15048124ee80a813f3da2fc0a286f
Alex Lima <alex.lima@jfl.com.br>
2026-05-19 16:28:12 -0300

    Implementado o CRUD do Autor. Obs.: Falta as resposta de falhas por completo.

--- S4_libraryapi/src/main/java/io/github/cursodsouza/libraryapi/LibraryapiApplication.java
+++ S4_libraryapi/src/main/java/io/github/cursodsouza/libraryapi/LibraryapiApplication.java
@@ -4,10 +4,12 @@ import io.github.cursodsouza.libraryapi.model.Autor;
 import io.github.cursodsouza.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
---
+++ S4_libraryapi/src/main/java/io/github/cursodsouza/libraryapi/controller/AutorController.java
@@ -0,0 +1,106 @@
+package io.github.cursodsouza.libraryapi.controller;
+
+import io.github.cursodsouza.libraryapi.controller.dto.AutorDTO;
+import io.github.cursodsouza.libraryapi.model.Autor;
+import io.github.cursodsouza.libraryapi.service.AutorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("autores")
+// http://localhost:8080/autores
+public class AutorController {
+    private final AutorService service; // Tera uma forma mais fácil/melhor do que essa
+    public AutorController(AutorService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor) {
+        Autor autorEntidade = autor.mapearParaAutor();
+        service.salvar(autorEntidade);
+
+        //http://localhost:8080/autores/02cda449-b1f0-4115-b088-34410069e6e0 (no headers de resposta resposta)
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+        if (autorOptional.isPresent()) {
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(
+                autor.getId(),
+                autor.getNome(),
+                autor.getDataNascimento(),
+                autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletar(autorOptional.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+           @RequestParam(value = "nome", required = false) String nome,
+           @RequestParam(value = "nacionalidade", required = false) String nacionalidade
+    ){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado.stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade()
+                )).collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(
+            @PathVariable("id") String id, @RequestBody AutorDTO dto) {
+
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var autor = autorOptional.get();
+
+        autor.setNome(dto.nome());
+        autor.setNacionalidade(dto.nacionalidade());
+        autor.setDataNascimento(dto.dataNascimento());
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+    }
+}
