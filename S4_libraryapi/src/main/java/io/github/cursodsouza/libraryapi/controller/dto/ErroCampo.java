f8bb35937481ead7b2713a8cb0865df36ba45e43
Alex Lima <alex.lima@jfl.com.br>
2026-05-20 16:46:13 -0300

    Correcao do commit anterior. Esta errado o comentario. O correto: 88. Criando um DTO padronizado de respostas de erro.

--- S4_libraryapi/src/main/java/io/github/cursodsouza/libraryapi/controller/dto/ErroCampo.java
+++ S4_libraryapi/src/main/java/io/github/cursodsouza/libraryapi/controller/dto/ErroCampo.java
@@ -2,7 +2,7 @@ package io.github.cursodsouza.libraryapi.controller.dto;
 
 import org.springframework.http.HttpStatus;
 import java.util.List;
-
+//
 public record ErroCampo (String campo, String erro){
     public static ErroResposta respostaPadra(String mensagem){
         return new ErroResposta(HttpStatus.BAD_REQUEST.value(), mensagem, List.of());
